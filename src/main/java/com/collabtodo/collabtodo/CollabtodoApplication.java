cd15128bb3f09a1ad4f69a001c8fe88c7daebe33
Martins Greizis <grei0003@hz.nl>
2023-08-17 15:58:08 +0300

    initial commit

---
+++ src/main/java/com/collabtodo/collabtodo/CollabtodoApplication.java
@@ -0,0 +1,13 @@
+package com.collabtodo.collabtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabtodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabtodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/collabtodo/collabtodo/CollabtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabtodo.collabtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabtodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
